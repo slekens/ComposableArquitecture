ad8cd2a3bc4ab4f5232aabfb4bb42bb2f44ed266
Abraham Abreu <slekens@gmail.com>
2022-11-09 21:19:42 -0600

    feat: Project creation, adding dependencies.

---
+++ ComposableArquitecture/ComposableArquitecture/ComposableArquitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArquitectureApp.swift
+//  ComposableArquitecture
+//
+//  Created by Abraham Abreu on 09/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArquitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
