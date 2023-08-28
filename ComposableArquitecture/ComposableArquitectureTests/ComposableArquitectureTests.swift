95c5d8b86fdab03f85abee6c50df481517e25f27
Abraham Abreu <slekens@gmail.com>
2023-08-27 18:41:07 -0600

    feat: Add tests for the counter feature.

--- ComposableArquitecture/ComposableArquitectureTests/ComposableArquitectureTests.swift
+++ ComposableArquitecture/ComposableArquitectureTests/ComposableArquitectureTests.swift
@@ -6,31 +6,29 @@
 //
 
 import XCTest
+import ComposableArchitecture
 @testable import ComposableArquitecture
 
+@MainActor
 final class ComposableArquitectureTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testFeatureIncrease() async throws {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
         }
+        await store.send(.increaseCounter) {
+            $0.counter = 1
+        }
+        XCTAssertEqual(store.state.counter, 1)
     }
-
+    
+    func testFeatureDecrease() async throws {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+        await store.send(.decreaseCounter) {
+            $0.counter = -1
+        }
+        XCTAssertEqual(store.state.counter, -1)
+    }
 }
